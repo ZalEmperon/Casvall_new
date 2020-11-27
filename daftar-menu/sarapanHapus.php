81384ce4bee650c092b8eb21790ec5414b8a2ce1
Bima <68883585+ZalEmperon@users.noreply.github.com>
2020-11-27 21:18:34 +0700

    KK

---
+++ daftar-menu/sarapanHapus.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../server.php';
+require_once '../functions.php';
+$id = $_GET['id'];
+if(isset($_GET["delete"])){
+	$hapus = "DELETE FROM sarapan WHERE id = '$id'";
+	if(mysqli_query($db, $hapus)){
+		echo "<script>
+				alert('meow')
+				window.location.href = 'sarapan.php'
+			</script>";
+	}
+}
+require '../templates/header.php';
+require '../templates/sidebar.php';
+require '../templates/topbar.php';
+?>
+
+<?php
+require '../templates/footer.php'; 
+?>
