ea4ee3e48835228bec6f376f318c8de161696a21
AbelChrist <abelimmanuelakristianto@gmail.com>
2020-11-27 12:41:04 +0700

    add admin dashboard menu

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<?php 
+require_once './server.php';
+// header template
+require './templates/header.php';
+// sidebar template
+require './templates/sidebar.php';
+// topbar template
+require './templates/topbar.php'; 
+?>
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">Hello World</h1>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+<?php
+// footer template
+ require './templates/footer.php'; 
+ ?>
